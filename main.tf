dd1ba9e7f355c24b2a5fe8e8a0cca8ad22e05d00
Brian Galeano <briangaleano@gmail.com>
2024-01-23 17:13:53 -0500

    Add PV for Uptime-Kuma

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
+# Resource Group
 resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
+# AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = var.cluster_name
   location            = azurerm_resource_group.aks_rg.location
@@ -25,11 +27,13 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 }
 
+# AKS Cluster Data
 data "azurerm_kubernetes_cluster" "current" {
   name                = azurerm_kubernetes_cluster.aks_cluster.name
   resource_group_name = azurerm_resource_group.aks_rg.name
 }
 
+# Kubernetes Provider Configuration
 provider "kubernetes" {
   host                   = data.azurerm_kubernetes_cluster.current.kube_config[0].host
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.current.kube_config[0].client_certificate)
@@ -37,6 +41,23 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.current.kube_config[0].cluster_ca_certificate)
 }
 
+# Persistent Volume Claim
+resource "kubernetes_persistent_volume_claim" "uptime_kuma_pvc" {
+  metadata {
+    name = "uptime-kuma-pvc"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+    storage_class_name = "default"
+  }
+}
+
+# Deploy Uptime-Kuma with a reference to the PVC for persistent storage
 resource "kubernetes_deployment" "uptime_kuma" {
   metadata {
     name = "uptime-kuma"
@@ -59,8 +80,23 @@ resource "kubernetes_deployment" "uptime_kuma" {
         container {
           image = "louislam/uptime-kuma:1"
           name  = "uptime-kuma"
+
           port {
             container_port = 3001
+          }
+
+          volume_mount {
+            mount_path = "/data"
+            name       = "uptime-kuma-storage"
+          }
+        }
+
+        # Define volumes using the PVC
+        volume {
+          name = "uptime-kuma-storage"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.uptime_kuma_pvc.metadata[0].name
           }
         }
       }
@@ -68,6 +104,7 @@ resource "kubernetes_deployment" "uptime_kuma" {
   }
 }
 
+# Expose Uptime-Kuma via a LoadBalancer Service
 resource "kubernetes_service" "uptime_kuma_service" {
   metadata {
     name = "uptime-kuma-service"
@@ -77,11 +114,10 @@ resource "kubernetes_service" "uptime_kuma_service" {
     selector = {
       app = "uptime-kuma"
     }
+    type = "LoadBalancer"
     port {
       port        = 80
       target_port = 3001
     }
-
-    type = "LoadBalancer"
   }
 }
